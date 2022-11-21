57e3fd434c5fb3410e194c6699c9ba8c133e5909
Will Parry <wsparry99@gmail.com>
2022-11-21 11:43:09 -0600

    Added inline and external Javascript.

---
+++ js/external-js.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("Hello from external JS");
+
+alert("Welcome to my website!");
+
+var userInput = prompt("What is your favorite color?");
+alert("Great! My favorite color is " + userInput + " too!");
+
